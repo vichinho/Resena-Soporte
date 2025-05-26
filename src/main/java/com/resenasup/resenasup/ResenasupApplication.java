348fe8bf90c18afe7e03ad9a137cb481b11d73e0
Vicente <vishoxcl@gmail.com>
2025-05-25 21:14:02 -0400

    first commit

---
+++ src/main/java/com/resenasup/resenasup/ResenasupApplication.java
@@ -0,0 +1,13 @@
+package com.resenasup.resenasup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResenasupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResenasupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/resenasup/resenasup/ResenasupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resenasup.resenasup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResenasupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
